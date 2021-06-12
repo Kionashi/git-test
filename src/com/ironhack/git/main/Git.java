828e6e41a55e0b12b259bbeb328ffdac783679af
Kionashi <cardozo.anibal@gmail.com>
2021-06-12 11:45:19 +0200

    fix typo in second message

--- src/com/ironhack/git/main/Git.java
+++ src/com/ironhack/git/main/Git.java
@@ -4,6 +4,6 @@ public class Git {
 
     public static void main(String[] args) {
         System.out.println("Hello git from new feature");
-        System.out.println("chang e");
+        System.out.println("change");
     }
 }
